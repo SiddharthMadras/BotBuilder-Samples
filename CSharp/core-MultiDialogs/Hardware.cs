0d3046c8966db3ff1c45ab2c1fd5b5fa65037e35
SiddharthMadras <sidhubillgates@gmail.com>
2016-11-24 23:40:44 +0530

    added hw

---
+++ CSharp/core-MultiDialogs/Hardware.cs
@@ -0,0 +1,20 @@
+namespace MultiDialogsBot
+{
+    using System;
+
+    [Serializable]
+    public class Hardware
+    {
+        public string Name { get; set; }
+
+        public int Rating { get; set; }
+
+        public int NumberOfReviews { get; set; }
+
+        public int PriceStarting { get; set; }
+
+        public string Image { get;  set; }
+
+        public string Location { get;  set; }
+    }
+}
